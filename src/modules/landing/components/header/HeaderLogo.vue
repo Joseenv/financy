633242bd30a9c1c8dda24b92c2dded46bc2104c9
Joseenv <jose229901@gmail.com>
2024-04-25 00:27:41 -0500

    [ Jose Navarro - feat ] : Adding Section Hero in Landing Page

--- src/modules/landing/components/header/HeaderLogo.vue
+++ src/modules/landing/components/header/HeaderLogo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <h1 class="text-base-light text-xl font-bold">
-      <span>Financial</span>
+      <span>Financy</span>
       <span class="text-brand-primary">.</span>
     </h1>
   </div>
